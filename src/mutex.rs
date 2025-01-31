e32f4f02717962194a8ef5041b67014d9e651e04
Da An <daan@Danila-2.local>
2025-01-31 06:36:15 +0400

    updates

--- src/mutex.rs
+++ src/mutex.rs
@@ -196,59 +196,6 @@ impl<T> DerefMut for SlowerMutexGuard<'_, T> {
 
 impl<T> !Send for SlowerMutexGuard<'_, T> {}
 
-//
-// #[derive(Debug)]
-// pub enum LockError<Guard> {
-//     WouldBlock,
-//     Poisoned(PoisonedLock<Guard>),
-// }
-//
-// impl<T> PartialEq for LockError<T> {
-//     fn eq(&self, other: &Self) -> bool {
-//         match self {
-//             LockError::WouldBlock => {
-//                 if let LockError::WouldBlock = other {
-//                     true
-//                 } else {
-//                     false
-//                 }
-//             },
-//             LockError::Poisoned(_)  => {
-//                 if let LockError::WouldBlock = other {
-//                     false
-//                 } else {
-//                     true
-//                 }
-//             },
-//         }
-//     }
-// }
-//
-// impl<T> fmt::Display for LockError<T> {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-//         match self {
-//             LockError::WouldBlock => write!(f, "Error: Can cause blocking"),
-//             LockError::Poisoned(_) => write!(f, "Error: Lock is poisoned"),
-//         }
-//     }
-// }
-//
-// pub struct PoisonedLock<Guard> {
-//     guard: Guard,
-// }
-//
-// impl<T> fmt::Display for PoisonedLock<T> {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-//         write!(f, "Lock is poisoned!")
-//     }
-// }
-//
-// impl<T> fmt::Debug for PoisonedLock<T> {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-//         write!(f, "Lock is poisoned!")
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use crate::mutex::{LockError, SlowerMutex};
